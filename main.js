47127a1d8671dfc3c6b2f6e2ba81f96c2141f948
Ofek Gila <flamingo.ofek@gmail.com>
2016-01-08 16:41:56 -0800

    Save cookies (cross fingers it works)

--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-const fs = require('fs');
 
 var dev = false;
-var browser_dimensions;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -24,18 +22,15 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
 
-  getBrowserDimensions(function(dimensions) {
-    browser_dimensions = dimensions;
-    createMainWindow();
-  });
+  createMainWindow();
 });
 
 function createMainWindow() {
   // Create the browser window.
 
   mainWindow = new BrowserWindow({
-    width: browser_dimensions.width,
-    height: browser_dimensions.height
+    width: 800,
+    height: 800
   });
 
   // and load the index.html of the app.
@@ -51,19 +46,5 @@ function createMainWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  });
-}
-
-function getBrowserDimensions(callback) {
-  loadFromFile("browser-dimensions.txt", function(err, data) {
-    if (err || data.length === 0)
-      callback([800, 800]);
-    else callback(JSON.parse(data));
-  });
-}
-
-function loadFromFile(relative_path, callback) {
-  fs.readFile(__dirname + "/" + relative_path, "utf8", function(err, data) {
-    callback(err, data);
   });
 }
